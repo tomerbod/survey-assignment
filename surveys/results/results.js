4b6bbd9b4ef543cccb16d3f8dab708b9d2aaa927
tomerbod <tomer.bodenshtein@is.com>
2023-01-10 11:03:46 +0200

    "add result class"

--- surveys/results/results.js
+++ surveys/results/results.js
@@ -14,93 +14,32 @@ const handlePaginationLinkClick = (event) => {
 //addResults get an "resultsToDisplay" that consists of results from the local storage and append them to "resultListContainer"
 const addResults = (resultsToDisplay, resultListContainer) => {
   resultsToDisplay.forEach((question, index) => {
-    const answerContainer = document.createElement("div");
-    answerContainer.classList.add(`answer`);
-    const answerTitle = document.createElement("h2");
-    answerTitle.innerHTML = question.text;
-    answerContainer.appendChild(answerTitle);
-
+    let result;
     if (question.type === "open") {
-      surveyResults?.forEach((result) => {
-        const expandButton = document.createElement("button");
-
-        expandButton.classList.add(`expand-button`);
-        expandButton.setAttribute("data-index", index);
-        expandButton.innerHTML = "Expand";
-        const answerDiv = document.createElement("div");
-
-        answerDiv.classList.add(`answers-${index}`);
-        answerDiv.setAttribute("style", "display: none;");
-        answerDiv.innerHTML = result[index];
-        expandButton.addEventListener("click", () => {
-          // show the answers when the expand button is clicked
-          if (answerDiv.style.display === "none") {
-            answerDiv.style.display = "block";
-            expandButton.innerHTML = "X";
-          } else {
-            expandButton.innerHTML = "Expand";
-            answerDiv.style.display = "none";
-          }
-        });
-        answerContainer.appendChild(expandButton);
-        answerContainer.appendChild(answerDiv);
+      result = new OpenResult({
+        title: question.text,
+        answers: surveyResults,
+        index: index,
       });
-    } else if (question.type === "closed" || question.type === "multi") {
-      // create an object to store the counts for each answer
-      const answerCounts = {};
-      if (question.type === "closed") {
-        surveyResults?.forEach((result) => {
-          const answer = result[index];
-          if (!answerCounts[answer]) {
-            answerCounts[answer] = 0;
-          }
-          answerCounts[answer]++;
-        });
-      } else {
-        surveyResults?.forEach((result) => {
-          const resultIndex =
-            index + NumberOfQuestionsInPage * (currentPage - 1);
-          const answer = result[resultIndex];
-          answer.forEach((answer) => {
-            if (!answerCounts[answer]) {
-              answerCounts[answer] = 0;
-            }
-            answerCounts[answer]++;
-          });
-        });
-      }
-
-      let answersDisplayed = [];
-      question.possibleAnswers.forEach((possibleAnswer) =>
-        answersDisplayed.push(answerCounts[possibleAnswer] ?? 0)
-      );
-
-      const ctx = document.createElement("canvas");
-
-      new Chart(ctx, {
-        type: "bar",
-        data: {
-          labels: question.possibleAnswers,
-          datasets: [
-            {
-              label: "# of Votes",
-              data: answersDisplayed,
-              borderWidth: 1,
-            },
-          ],
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true,
-            },
-          },
-        },
-      });
-
-      answerContainer.appendChild(ctx);
     }
-    resultListContainer.appendChild(answerContainer);
+    if (question.type === "closed") {
+      result = new ClosedResult({
+        title: question.text,
+        answers: surveyResults,
+        index: index,
+        possibleAnswers: question.possibleAnswers,
+      });
+    }
+    if (question.type === "multi") {
+      result = new MultiResult({
+        title: question.text,
+        answers: surveyResults,
+        index: index + NumberOfQuestionsInPage * (currentPage - 1),
+        possibleAnswers: question.possibleAnswers,
+      });
+    }
+
+    resultListContainer.appendChild(result.render());
   });
 };
 
