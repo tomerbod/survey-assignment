582a50cc99e33423cfb8638de94d82e7d9c62f6c
tomerbod <tomer.bodenshtein@is.com>
2023-01-17 14:12:01 +0200

    "forgot to add this"

--- classes/Result/multiResultClass.js
+++ classes/Result/multiResultClass.js
@@ -8,18 +8,7 @@ export class MultiResult extends Result {
 
   render() {
     const answerContainer = super.render();
-    const answerCounts = {};
-    this.answers?.forEach((result) => {
-      const answer = result[this.index];
-      answer.forEach((answer) => {
-        if (!answerCounts[answer]) {
-          answerCounts[answer] = 0;
-        }
-        answerCounts[answer]++;
-      });
-    });
-
-    const answerCounts2 = this.answers?.reduce((accumulator, currentValue) => {
+    const answerCounts = this.answers?.reduce((accumulator, currentValue) => {
       currentValue[this.index]?.forEach((answer) => {
         if (!accumulator[answer]) {
           accumulator[answer] = 0;
@@ -29,9 +18,6 @@ export class MultiResult extends Result {
 
       return accumulator;
     }, {});
-
-    console.log({ answerCounts });
-    console.log({ answerCounts2 });
 
     answerContainer.appendChild(
       this.createGraph(this.possibleAnswers, answerCounts)
